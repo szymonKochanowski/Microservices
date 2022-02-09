dfdd21ad20f717d78d3f2a7042dec7a4c0ed38a2
Szymon Kochanowski <szymon.kochanowski@fdmgroup.com>
2022-02-09 20:53:24 +0100

    added option to withrawn funds

---
+++ bank-account/bankacc.cmd.api/src/main/java/com/springbank/bankacc/cmd/api/controllers/CloseAccountController.java
@@ -0,0 +1,9 @@
+package com.springbank.bankacc.cmd.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/closeBankAccount")
+public class CloseAccountController {
+}
